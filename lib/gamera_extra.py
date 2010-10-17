434dbcf02f07c32c6733f028de7e1d9ab0b8ff7b
Jakub Wilk <jwilk@jwilk.net>
2010-10-17 12:12:07 +0200

    gamera_extra: implement a work-around for memory leak.

--- lib/gamera_extra.py
+++ lib/gamera_extra.py
@@ -16,9 +16,10 @@
 import os
 import re
 import sys
+import warnings
 
 from gamera.core import load_image as _load_image
-from gamera.core import init_gamera as init
+from gamera.core import init_gamera as _init
 from gamera.core import Image, RGB, GREYSCALE, ONEBIT, Dim, RGBPixel
 from gamera.plugins.pil_io import from_pil as _from_pil
 
@@ -76,5 +77,49 @@ methods = _load_methods()
 
 def to_pil_1bpp(image):
     return image.to_greyscale().to_pil()
+
+def _decref(o):
+    '''
+    Forcibly set refcount of the object to 1.
+    '''
+    import ctypes
+    libc = ctypes.cdll.LoadLibrary(None)
+    memmove = libc.memmove
+    memmove.argtypes = [ctypes.py_object, ctypes.py_object, ctypes.c_size_t]
+    memmove.restype = ctypes.py_object
+    return memmove(o, None, 0)
+
+def _monkeypatch_to_raw_string():
+    '''
+    Monkey-patch to _to_raw_string plugin function to return objects with
+    correct refcounts.
+    '''
+    from gamera.plugins import _string_io
+    old_to_raw_string = _string_io._to_raw_string
+    def fixed_to_raw_string(*args, **kwargs):
+        o = old_to_raw_string(*args, **kwargs)
+        assert sys.getrefcount(o) == 3
+        o = _decref(o)
+        assert sys.getrefcount(o) == 2
+        return o
+    _string_io._to_raw_string = fixed_to_raw_string
+    Image._to_raw_string = fixed_to_raw_string
+
+def init():
+    result = _init()
+    test_image = Image((0, 0), (5, 5), RGB)
+    test_string = test_image._to_raw_string()
+    refcount = sys.getrefcount(test_string)
+    if refcount == 3:
+        # See: http://tech.groups.yahoo.com/group/gamera-devel/message/2068
+        warnings.warn(RuntimeWarning('Working around memory leak in the Gamera library'), stacklevel=2)
+        _monkeypatch_to_raw_string()
+        fixed_test_string = test_image._to_raw_string()
+        test_image.to_pil()
+        assert sys.getrefcount(fixed_test_string) == 2
+        assert test_string == fixed_test_string
+    else:
+        assert refcount == 2
+    return result
 
 # vim:ts=4 sw=4 et
