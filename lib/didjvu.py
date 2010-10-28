f2ed4971262d43655927be0867264fd1b5395d04
Jakub Wilk <jwilk@jwilk.net>
2010-10-28 22:10:53 +0200

    Avoid double spaces in the copyright statement.

--- lib/didjvu.py
+++ lib/didjvu.py
@@ -9,7 +9,7 @@
 #
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 
 from __future__ import with_statement
