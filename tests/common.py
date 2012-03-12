9ccfb06ee709f51c65697175524c2babd7f6b124
Jakub Wilk <jwilk@jwilk.net>
2012-03-12 22:28:58 +0100

    tests: add tests for utils.

--- tests/common.py
+++ tests/common.py
@@ -1,6 +1,6 @@
 # encoding=UTF-8
 
-# Copyright © 2012 Jakub Wilk <jwilk@jwilk.net>
+# Copyright © 2010, 2011, 2012 Jakub Wilk <jwilk@jwilk.net>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -11,7 +11,9 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 
+import contextlib
 import re
+import sys
 
 from nose import SkipTest
 from nose.tools import (
@@ -32,5 +34,35 @@ def assert_rfc3339_timestamp(timestamp):
         '^[0-9]{4}(-[0-9]{2}){2}T[0-9]{2}(:[0-9]{2}){2}([+-][0-9]{2}:[0-9]{2}|Z)$',
         timestamp
     )
+
+@contextlib.contextmanager
+def exception(exc_type, string=None, regex=None, callback=None):
+    if sum(x is not None for x in (string, regex, callback)) != 1:
+        raise ValueError('exactly one of: string, regex, callback must be provided')
+    if string is not None:
+        def callback(exc):
+            assert_equal(str(exc), string)
+    elif regex is not None:
+        def callback(exc):
+            assert_regexp_matches(regex, str(exc))
+    try:
+        yield None
+    except exc_type:
+        _, exc, _ = sys.exc_info()
+        callback(exc)
+    else:
+        message = '%s was not raised' % exc_type.__name__
+        raise AssertionError(message)
+
+@contextlib.contextmanager
+def interim(obj, **override):
+    copy = dict((key, getattr(obj, key)) for key in override)
+    for key, value in override.iteritems():
+        setattr(obj, key, value)
+    try:
+        yield
+    finally:
+        for key, value in copy.iteritems():
+            setattr(obj, key, value)
 
 # vim:ts=4 sw=4 et
---
+++ tests/test_utils.py
@@ -0,0 +1,49 @@
+# encoding=UTF-8
+
+# Copyright © 2010, 2011 Jakub Wilk <jwilk@jwilk.net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 dated June, 1991.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+
+from __future__ import with_statement
+
+import sys
+
+from . common import (
+	interim,
+    exception,
+)
+
+from lib import utils
+
+class test_enhance_import():
+
+    @classmethod
+    def setup_class(cls):
+        sys.modules['nonexistent'] = None
+
+    def test_debian(self):
+        with interim(utils, debian=True):
+            with exception(ImportError, 'No module named nonexistent; please install the python-nonexistent package'):
+                try:
+                    import nonexistent
+                except ImportError, ex:
+                    utils.enhance_import_error(ex, 'PyNonexistent', 'python-nonexistent', 'http://pynonexistent.example.net/')
+                    raise
+
+    def test_nondebian(self):
+        with interim(utils, debian=False):
+            with exception(ImportError, 'No module named nonexistent; please install the PyNonexistent package <http://pynonexistent.example.net/>'):
+                try:
+                    import nonexistent
+                except ImportError, ex:
+                    utils.enhance_import_error(ex, 'PyNonexistent', 'python-nonexistent', 'http://pynonexistent.example.net/')
+                    raise
+
+# vim:ts=4 sw=4 et
