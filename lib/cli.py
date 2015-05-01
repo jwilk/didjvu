40025d363741ac0baea71954eb44e71dcced4aea
Jakub Wilk <jwilk@jwilk.net>
2015-05-01 14:24:11 +0200

    Update the argparse URL.

--- lib/cli.py
+++ lib/cli.py
@@ -18,7 +18,7 @@ from . import utils
 try:
     import argparse
 except ImportError, ex:
-    utils.enhance_import_error(ex, 'argparse', 'python-argparse', 'https://code.google.com/p/argparse/')
+    utils.enhance_import_error(ex, 'argparse', 'python-argparse', 'https://pypi.python.org/pypi/argparse')
     raise
 
 from . import djvu_extra as djvu
