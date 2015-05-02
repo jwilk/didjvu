9d91f42bd0e4d881e4733c5e32c3961dd93632cc
Jakub Wilk <jwilk@jwilk.net>
2015-05-02 18:34:36 +0200

    __init__: add no-coverage annotations.

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,6 +1,6 @@
 # encoding=UTF-8
 
-# Copyright © 2009-2013 Jakub Wilk <jwilk@jwilk.net>
+# Copyright © 2009-2015 Jakub Wilk <jwilk@jwilk.net>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,8 +14,8 @@
 import sys
 
 if sys.version_info < (2, 6):
-    raise RuntimeError('Python >= 2.6 is required')
+    raise RuntimeError('Python >= 2.6 is required')  # <no-coverage>
 if sys.version_info >= (3, 0):
-    raise RuntimeError('Python 2.X is required')
+    raise RuntimeError('Python 2.X is required')  # <no-coverage>
 
 # vim:ts=4 sts=4 sw=4 et
