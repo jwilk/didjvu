b4114749bad7f3dd5c625445cf9260befb1c3f46
Jakub Wilk <jwilk@jwilk.net>
2015-05-02 09:54:36 +0200

    Update copyright information.

--- lib/xmp/namespaces.py
+++ lib/xmp/namespaces.py
@@ -1,5 +1,16 @@
 # encoding=UTF-8
 
+# Copyright © 2012 Jakub Wilk <jwilk@jwilk.net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 dated June, 1991.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# General Public License for more details.
+
 didjvu = 'http://jwilk.net/software/didjvu#'
 
 dc = 'http://purl.org/dc/elements/1.1/'
