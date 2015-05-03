dd9af992c4fd0e470c2263c96bea18e26a6c3104
Jakub Wilk <jwilk@jwilk.net>
2015-05-03 22:52:20 +0200

    Bump version to 0.4.

--- lib/version.py
+++ lib/version.py
@@ -11,7 +11,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 
-__version__ = '0.2.10'
+__version__ = '0.4'
 
 def get_software_agent():
     try:
