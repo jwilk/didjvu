8407fc7b815e15c4961aed7c29b2174e9ef5e177
Jakub Wilk <jwilk@jwilk.net>
2015-05-10 19:27:30 +0200

    tests/test_fs: add test for fs.copy_file().

--- tests/test_fs.py
+++ tests/test_fs.py
@@ -11,11 +11,27 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 # General Public License for more details.
 
+import cStringIO as io
+
 from . common import (
     assert_equal,
+    interim,
 )
 
 from lib import fs
+
+def test_copy_file():
+    def t(s):
+        input_file = io.StringIO(s)
+        output_file = io.StringIO()
+        length = fs.copy_file(input_file, output_file)
+        assert_equal(output_file.tell(), length)
+        output_file.seek(0)
+        r = output_file.read()
+        assert_equal(s, r)
+    t('eggs')
+    with interim(fs, _block_size=1):
+        t('eggs' + 'spam' * 42)
 
 def test_replace_ext():
     r = fs.replace_ext('eggs', 'spam')
