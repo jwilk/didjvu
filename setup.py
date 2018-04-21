jwilk/didjvu

commit a3816dae3dd8f6aab0eb6ffa19f1f525a2636fbc

setup: get version from changelog.

setup.py
@@ -37,7 +37,10 @@ except ImportError:
 else:
     distutils644.install()
 
-from lib.version import __version__
+def get_version():
+    with open('doc/changelog', 'r') as file:
+        line = file.readline()
+    return line.split()[1].strip('()')
 
 data_files = []
 
@@ -160,7 +163,7 @@ Topic :: Multimedia :: Graphics
 
 distutils.core.setup(
     name='didjvu',
-    version=__version__,
+    version=get_version(),
     license='GNU GPL 2',
     description='DjVu encoder with foreground/background separation',
     long_description=__doc__.strip(),

commit 5ae92bf88fcd0b7d64867e2df0adb1bbcf810589

setup: add Python 3 version check.
Fixes: a3816dae3dd8f6aab0eb6ffa19f1f525a2636fbc

setup.py
@@ -19,6 +19,7 @@ which it can then encode into a DjVu file.
 '''
 
 b''  # Python >= 2.6 is required
+exec b''  # Python 2.X is required
 
 import glob
 import os