jwilk/didjvu

commit 03a53477a06735ac58d75f94b97ad94b5755f1f3

Defer logging setup until after CLI args are parsed.

lib/didjvu.py
@@ -191,9 +191,9 @@ class main():
     )
 
     def __init__(self):
-        setup_logging()
         parser = cli.ArgumentParser(gamera.methods, default_method='djvu')
         parser.parse_args(actions=self)
+        setup_logging()
 
     def check_common(self, o):
         if len(o.masks) == 0:

commit 5103f6955e876122f3090b66d530da9a1058754a

Setup logging before using it.
Fixes: 03a53477a06735ac58d75f94b97ad94b5755f1f3

Fixes:

    Traceback (most recent call last):
      File ".../didjvu", line 25, in <module>
        didjvu.main()
      File ".../lib/didjvu.py", line 195, in __init__
        parser.parse_args(actions=self)
      File ".../lib/cli.py", line 339, in parse_args
        return action(o)
      File ".../lib/didjvu.py", line 344, in bundle
        self.check_single_output(o)
      File ".../lib/didjvu.py", line 251, in check_single_output
        self.check_common(o)
      File ".../lib/didjvu.py", line 207, in check_common
        assert logger is not None
    AssertionError

lib/didjvu.py
@@ -193,7 +193,6 @@ class main():
     def __init__(self):
         parser = cli.ArgumentParser(gamera.methods, default_method='djvu')
         parser.parse_args(actions=self)
-        setup_logging()
 
     def check_common(self, o):
         if len(o.masks) == 0:
@@ -203,6 +202,7 @@ class main():
                 len(o.input),
                 len(o.masks)
             )
+        setup_logging()
         ipc_logger = logging.getLogger('didjvu.ipc')
         assert logger is not None
         log_level = {